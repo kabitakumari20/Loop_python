e2d5ca480a44b0df6e8c9afda5140d872cedd3b8
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-09-29 09:22:11 +0530

    Add files via upload

---
+++ parteen_tirvuj.py
@@ -0,0 +1,24 @@
+# i=0
+# while i<5:
+#     print(" "*(5-i)+"*",end=" ")
+#     j=0
+#     while j<i:
+#         print("*",end=" ")
+#         j=j+1
+#     print()
+#     i=i+1
+
+
+
+i=1
+while(i<=5):
+	b=1
+	while(b<=5-i):
+		print(end=" ")
+		b=b+1
+	j=1
+	while(j<=i):
+		print("*", end=" ")
+		j=j+1
+	print()
+	i=i+1
